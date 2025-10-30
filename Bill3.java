9704fff430d02d3552e3efd547a8d7fefa4d35af
Loren Chen <loren.hen@post.runi.ac.il>
2025-10-30 14:26:18 +0200

    done!

--- Bill3.java
+++ Bill3.java
@@ -5,18 +5,18 @@ public class Bill3 {
     public static void main(String[] args) {
         // To get you started, here is the first line in the program:
         Scanner input = new Scanner(System.in);
-        String name1;
-        String name2;
-        String name3;
-        double check;
-        double payment;
-        System.out.print("enter your names : ");
+        String name1; // variable for the first name
+        String name2; // variable for the second name
+        String name3; // variable for the third name
+        double check; // variable for the check value
+        double payment; // variable for the payment for each one 
+        System.out.print("enter your names : "); // statement for all 3 name inputs
         name1= input.nextLine();
         name2= input.nextLine();
         name3= input.nextLine();
-        System.out.print("enter your check : ");
+        System.out.print("enter your check : "); // statement for the check input
         check= input.nextDouble();
-        payment= Math.ceil((check/3)); 
-        System.out.println("Dear "+ name3 + ", " + name2 + ", and " + name1 + ": Pay " + payment + " Shekels each.");
+        payment= Math.ceil((check/3)); // payment caculation
+        System.out.println("Dear "+ name3 + ", " + name2 + ", and " + name1 + ": Pay " + payment + " Shekels each."); // output stracture
     }
 }
